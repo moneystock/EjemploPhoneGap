19d39d01ea1f8b604993a9e1003a0358974e788b
Alvaro Galvis Reyes <alvarogalvisreyes@Alvaros-Mac.local>
2011-08-26 18:31:23 -0500

    Initial Commit

---
+++ EjemploPhoneGap/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  EjemploPhoneGap
+//
+//  Created by Alvaro Galvis Reyes on 26/08/11.
+//  Copyright Nethub Media - Go2Do 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
